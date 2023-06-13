2fff7403f485380d85b93b9c6610c9e8861a3afa
Rambod <gh.rambod@gmail.com>
2023-06-13 10:14:06 +0300

    Refactor Home Page component: Removed description, added grid view
    Explanation:
    
    Removed the description section from the template of the Home Page component.
    Implemented a grid view layout for displaying comics, with 6 items per row.
    Updated the styling to set fixed height and width for the comic items.
    Added a placeholder for the banner image at the top.
    Rewrote the code to inject the modifications without altering the existing script and method sections.

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
+    <div class="banner">
+      <!-- Placeholder for the banner image -->
+    </div>
     <h1>Home Page</h1>
     <div v-if="comics.length === 0">
       Loading comics...
     </div>
     <div v-else>
-      <div v-for="comic in comics" :key="comic.id">
-        <h2>{{ comic.title }}</h2>
-        <img :src="comic.thumbnail" alt="Comic Thumbnail">
-        <p>{{ comic.description }}</p>
-        <p>Creators:</p>
-        <ul>
-          <li v-for="creator in comic.creators" :key="creator.id">
-            {{ creator.name }}
-          </li>
-        </ul>
-        <button @click="addToFavorites(comic)" :disabled="isComicFavorite(comic.id)">
-          {{ isComicFavorite(comic.id) ? 'Added to Favorites' : 'Add to Favorites' }}
-        </button>
-        <router-link :to="`/comic/${comic.id}`">View Details</router-link>
-        <hr>
+      <div class="grid">
+        <div v-for="(comic, index) in comics" :key="comic.id" :style="getComicStyle(index)">
+          <img :src="comic.thumbnail" alt="Comic Thumbnail">
+          <button @click="addToFavorites(comic)" :disabled="isComicFavorite(comic.id)">
+            {{ isComicFavorite(comic.id) ? 'Added to Favorites' : 'Add to Favorites' }}
+          </button>
+          <router-link :to="`/comic/${comic.id}`">View Details</router-link>
+        </div>
       </div>
     </div>
 
@@ -64,12 +60,7 @@ export default {
         const comics = response.data.data.results.map((comic) => ({
           id: comic.id,
           title: comic.title,
-          description: comic.description || 'No description available.',
           thumbnail: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
-          creators: comic.creators.items.map((creator) => ({
-            id: creator.resourceURI,
-            name: creator.name,
-          })),
         }));
         this.$store.dispatch('setComics', comics);
       } catch (error) {
@@ -79,6 +70,16 @@ export default {
     addToFavorites(comic) {
       this.addFavoriteComic(comic);
     },
+    getComicStyle(index) {
+      const row = Math.floor(index / 6);
+      const col = index % 6;
+      return {
+        gridRow: `${row + 1}`,
+        gridColumn: `${col + 1}`,
+        height: '200px', // Set your desired height
+        width: '150px', // Set your desired width
+      };
+    },
   },
   mounted() {
     this.fetchComics();
@@ -87,6 +88,27 @@ export default {
 </script>
 
 <style scoped>
+.banner {
+  /* Add styles for the banner image placeholder */
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 10px;
+}
+
+.grid > div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.grid img {
+  height: 100px; /* Set your desired image height */
+  width: 100px; /* Set your desired image width */
+}
+
 .favorites {
   display: flex;
   align-items: center;
