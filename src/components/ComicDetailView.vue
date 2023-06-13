8222882d2e105645eceec1043760356185b4c971
Rambod <gh.rambod@gmail.com>
2023-06-13 15:14:02 +0300

    Update ComicDetailView component
    Description:
    
    Moved the back button to the left side of the page.
    Added the comic image on the left side and limited its height to a maximum of 60% of the display size.
    Placed the comic details (title, description, creators) on the right side.
    Replaced the favorite button with a toggle button that uses heart icons to indicate the comic's favorite status.
    Updated the styles to align the components and apply the desired design.

--- src/components/ComicDetailView.vue
+++ src/components/ComicDetailView.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
     <h1>Comic Detail Page</h1>
-    <BackButton />
-    <div v-if="comic">
-      <h2>{{ comic.title }}</h2>
-      <img :src="comic.thumbnail" alt="Comic Thumbnail">
-      <p>{{ comic.description }}</p>
-      <p>Creators:</p>
-      <ul>
-        <li v-for="creator in comic.creators" :key="creator.id">
-          {{ creator.name }}
-        </li>
-      </ul>
-      <button @click="addToFavorites(comic)" :disabled="isComicFavorite(comic.id)">
-        {{ isComicFavorite(comic.id) ? 'Added to Favorites' : 'Add to Favorites' }}
-      </button>
+    <div class="back-button">
+      <BackButton />
+    </div>
+    <div class="comic-details" v-if="comic">
+      <div class="comic-image">
+        <img :src="comic.thumbnail" :style="getComicImageStyle" alt="Comic Thumbnail">
+      </div>
+      <div class="comic-info">
+        <h2>{{ comic.title }}</h2>
+        <p>{{ comic.description }}</p>
+        <p>Creators:</p>
+        <ul>
+          <li v-for="creator in comic.creators" :key="creator.id">
+            {{ creator.name }}
+          </li>
+        </ul>
+        <button @click="toggleFavorite(comic)" class="favorite-button">
+          <i :class="isComicFavorite(comic.id) ? 'fas fa-heart' : 'far fa-heart'"></i>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -24,27 +30,92 @@ import { mapGetters, mapActions } from 'vuex';
 import BackButton from './BackButton.vue';
 
 export default {
-    computed: {
-        ...mapGetters(["favoriteComics", "isComicFavorite"]),
-        comic() {
-            const comicId = parseInt(this.$route.params.id); // Convert the id to an integer
-            return this.$store.state.comics.find((comic) => comic.id === comicId);
-        },
+  computed: {
+    ...mapGetters(['favoriteComics', 'isComicFavorite']),
+    comic() {
+      const comicId = parseInt(this.$route.params.id); // Convert the id to an integer
+      return this.$store.state.comics.find((comic) => comic.id === comicId);
     },
-    methods: {
-        ...mapActions(["addFavoriteComic"]),
-        addToFavorites(comic) {
-            this.addFavoriteComic(comic);
-        },
+    getComicImageStyle() {
+      return {
+        maxHeight: '80vh',
+      };
     },
-    // write mounted with console log inside to be sure we mount it
-    mounted() {
-        console.log("mounted");
+  },
+  methods: {
+    ...mapActions(['addFavoriteComic', 'removeFavoriteComic']),
+    toggleFavorite(comic) {
+      if (this.isComicFavorite(comic.id)) {
+        this.removeFavoriteComic(comic.id);
+      } else {
+        this.addFavoriteComic(comic);
+      }
     },
-    components: { BackButton }
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  components: { BackButton },
 };
 </script>
 
 <style scoped>
-/* Add component styles here */
+.back-button {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+
+.comic-details {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-top: 20px;
+}
+
+.comic-image {
+  flex: 1;
+}
+
+.comic-image img {
+  width: 100%;
+}
+
+.comic-info {
+  flex: 1;
+  margin-left: 20px;
+}
+
+.comic-info h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.comic-info p {
+  margin-bottom: 10px;
+}
+
+.favorite-button {
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  font-size: 24px;
+  cursor: pointer;
+}
+
+.favorite-button i {
+  color: #000000;
+}
+
+.favorite-button i:hover {
+  color: #ff0000;
+}
+
+.favorite-button i.fas {
+  /* Add styles for the filled heart icon */
+}
+
+.favorite-button i.far {
+  /* Add styles for the outlined heart icon */
+}
 </style>
